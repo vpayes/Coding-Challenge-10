5650f634481ae719864d84958cae82c7d49c3406
vpayes <vanesa_payes@outlook.com>
2024-10-20 23:34:26 -0400

    Add event listener to update price on size selection

---
+++ main.js
@@ -0,0 +1,20 @@
+// Task 2
+document.addEventListener("html"), () => {
+    const sizeSelect = document.getElementById("size-select");
+    const priceDisplay = document.getElementById("product-price");
+    const purchaseButton = document.getElementById("purchase-button");
+
+    sizeSelect.addEventListener("size"), (event) => {
+        const selectedOption = event.target.selectedOptions[0];
+        const newPrice = selectedOption.getAttribute("data-price");
+        priceDisplay.textContent = `$${newPrice}`;
+
+        const stockStatus = selectedOption.getAttribute("data-stock");
+        if (stockStatus === "out-of-stock") {
+            purchaseButton.disabled = true;
+        } else {
+            purchaseButton.disabled = false;
+        }
+    }
+    }
+    
